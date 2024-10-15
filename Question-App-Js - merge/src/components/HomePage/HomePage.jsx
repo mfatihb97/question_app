d99e40b108cbe226104f69e08821b22371b724c4
mfatihb97 <mfatihbatur@gmail.com>
2024-10-15 18:37:45 +0300

    commit -böcü

--- Question-App-Js - merge/src/components/HomePage/HomePage.jsx
+++ Question-App-Js - merge/src/components/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ export const HomePage = () => {
         <div className='container-fluid bg-success-subtle d-flex align-items-center justify-content-center flex-grow-1'>
             HomePage 2
             HomePage 5
+            HomePage 8
         </div>
     )
 }
